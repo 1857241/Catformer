3496b26a76fb859451c570ec03bd902ebc9be822
1857241 <1857241@01-19766.pusd.dom>
2023-05-25 09:56:24 -0700

    Created frame

--- Catformer/src/Driver.java
+++ Catformer/src/Driver.java
@@ -1,9 +1,14 @@
-
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+ 
 public class Driver {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("hello world");
-	}
-
+  public static void main(String[] args){
+	  JFrame frame = new JFrame();
+	  frame.setSize(800,600);
+	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  frame.setTitle("Catformer");
+	  frame.setVisible(true);
+  }
 }
