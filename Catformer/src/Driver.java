102185d2874532d4c74c2bf6fdb499c35662e1e4
1857241 <1857241@01-19766.pusd.dom>
2023-05-26 09:31:13 -0700

    Paint method fixed

--- Catformer/src/Driver.java
+++ Catformer/src/Driver.java
@@ -1,14 +1,60 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
- 
-public class Driver {
-  public static void main(String[] args){
-	  JFrame frame = new JFrame();
-	  frame.setSize(800,600);
-	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	  frame.setTitle("Catformer");
-	  frame.setVisible(true);
-  }
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.Timer;
+
+public class Driver extends JPanel implements ActionListener, KeyListener{
+	
+	public static Player chocola;
+	
+	public void paint(Graphics g) {
+		super.paintComponent(g);
+		chocola.paint(g);
+	}
+	public static void main(String[] args){
+		chocola = new Player();
+	  	new Driver();
+	}
+	
+	public Driver() {
+		JFrame frame = new JFrame("Nekopara");
+		frame.setSize(800,600);
+		frame.add(this);
+		frame.addKeyListener(this);
+	  	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  	frame.setTitle("Catformer");
+	  	Timer t = new Timer(16, this);
+	  	t.start();
+	  	frame.setVisible(true);
+	  	
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		repaint();
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+  
 }
--- Catformer/src/Player.java
+++ Catformer/src/Player.java
@@ -1,4 +1,51 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
 
 public class Player {
 	
+	
+	private Image Sprite;
+	private AffineTransform tx;
+	
+	private int x, y;
+	
+	
+	public Player() {
+		Sprite = getImage("/imgs/chocolaWalking.gif");
+		tx = AffineTransform.getTranslateInstance(0, 0);
+		init(x, y);
+		
+	}
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(20, 20);
+	}
+	public void update() {
+		tx.setToTranslation(x, y);
+		tx.scale(20, 20);
+	}
+	
+	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		
+		update();
+		g2.drawImage(Sprite, tx, null);
+	} 
+	
+	
+	protected Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Player.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
 }
