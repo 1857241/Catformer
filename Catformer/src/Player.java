07a76ba6a5a8a59ad99bc089ef89aa32a139d8d3
1857241 <1857241@01-19766.pusd.dom>
2023-05-30 09:00:03 -0700

    Deleted empty classes

--- Catformer/src/Player.java
+++ Catformer/src/Player.java
@@ -2,40 +2,37 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.geom.AffineTransform;
 import java.net.URL;
 
-public class Player {
-	
-	
+public class Player implements KeyListener {
 	private Image Sprite;
 	private AffineTransform tx;
-	
 	private int x, y;
-	
-	
+	private int vx, vy;
 	public Player() {
 		Sprite = getImage("/imgs/chocolaWalking.gif");
-		tx = AffineTransform.getTranslateInstance(0, 0);
-		init(x, y);
-		
+		tx = AffineTransform.getTranslateInstance(0, 600);
+		init(0, 600);
 	}
 	private void init(double a, double b) {
 		tx.setToTranslation(a, b);
-		tx.scale(20, 20);
+		tx.scale(2, 2);
 	}
 	public void update() {
 		tx.setToTranslation(x, y);
-		tx.scale(20, 20);
+		tx.scale(2, 2);
 	}
-	
+	public void move() {
+		
+	}
 	public void paint(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
-		
 		update();
 		g2.drawImage(Sprite, tx, null);
 	} 
-	
 	
 	protected Image getImage(String path) {
 		Image tempImage = null;
@@ -47,5 +44,20 @@ public class Player {
 			e.printStackTrace();
 		}
 		return tempImage;
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
